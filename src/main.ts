05d5093385a0bf80f108cf30bde6064db8fbcbc6
Chris <capacheco.22@est.ucab.edu.ve>
2024-02-28 14:20:28 -0400

    feat: create, update and list contacts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PartialGraphHost } from '@nestjs/core';
-
-import { writeFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    snapshot: true,
-    abortOnError: false,
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-
-bootstrap().catch((err) => {
-  writeFileSync('graph.json', PartialGraphHost.toString() ?? '');
-  console.error(err);
-  process.exit(1);
-});
+bootstrap();
