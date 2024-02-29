c3c32b2a6e6ab947bd095a644ff5aabbe3ba8d84
Chris <capacheco.22@est.ucab.edu.ve>
2024-02-29 00:43:34 -0400

    feat: home page

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: ['http://localhost:4200', 'https://gest-client.vercel.app'],
     allowedHeaders: '*',
-    origin: '*',
     credentials: true,
   });
   await app.listen(3000);
